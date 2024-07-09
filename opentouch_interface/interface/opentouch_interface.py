ec9e0b8b52e1bb39b0e920eb9b8b479f812e72bc
ature <kingature@gmail.com>
2024-07-09 23:37:37 +0200

    Using dynamic imports in opentouch_interface.py

--- opentouch_interface/interface/opentouch_interface.py
+++ opentouch_interface/interface/opentouch_interface.py
@@ -1,6 +1,3 @@
-from opentouch_interface.interface.sensors.digit import DigitSensor
-from opentouch_interface.interface.sensors.file_sensor import FileSensor
-from opentouch_interface.interface.sensors.gelsight_mini import GelsightMiniSensor
 from opentouch_interface.interface.touch_sensor import TouchSensor
 
 
@@ -8,10 +5,28 @@ class OpentouchInterface:
 
     def __new__(cls, sensor_type: 'TouchSensor.SensorType', *args, **kwargs):
         if sensor_type == TouchSensor.SensorType.DIGIT:
-            return DigitSensor()
+            try:
+                from opentouch_interface.interface.sensors.digit import DigitSensor
+                return DigitSensor()
+            except ImportError as e:
+                raise ImportError("DigitSensor dependencies are not installed. Please install them using 'pip install "
+                                  "digit-interface'") from e
+
         elif sensor_type == TouchSensor.SensorType.GELSIGHT_MINI:
-            return GelsightMiniSensor()
+            try:
+                from opentouch_interface.interface.sensors.gelsight_mini import GelsightMiniSensor
+                return GelsightMiniSensor()
+            except ImportError as e:
+                raise ImportError("GelsightMiniSensor dependencies are not installed. Please install them using 'pip "
+                                  "install gelsight-mini-interface'") from e
+
         elif sensor_type == TouchSensor.SensorType.FILE:
-            return FileSensor()
+            try:
+                from opentouch_interface.interface.sensors.file_sensor import FileSensor
+                return FileSensor()
+            except ImportError as e:
+                raise ImportError("FileSensor dependencies are not installed. Please install them using 'pip install "
+                                  "file-sensor-interface'") from e
+
         else:
             raise ValueError(f'Invalid sensor type {sensor_type}')
